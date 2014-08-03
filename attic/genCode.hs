28e22f86910c9e0781ea65862150d49f664f44d8
Takahiro HIMURA <taka@himura.jp>
2014-08-03 15:53:06 +0900

    Add attic

---
+++ attic/genCode.hs
@@ -0,0 +1,95 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE Arrows #-}
+
+import qualified Data.ByteString.Lazy as L
+import qualified Data.ByteString.Lazy.Char8 as L8
+import Data.Char
+import Data.Maybe
+import qualified Data.Text as T
+import qualified Data.Text.IO as T
+import Network.HTTP.Conduit
+import System.Environment
+import Text.Shakespeare.Text (st)
+import Text.XML.HXT.Core hiding (when)
+import Text.XML.HXT.XPath
+
+data Field = Field
+    { fieldName :: String
+    , fieldHaskellName :: Maybe String
+    , fieldMappedType :: String
+    , fieldNullable :: Bool
+    } deriving Show
+
+parseXML :: String -> IOStateArrow s b XmlTree
+parseXML = readString [ withValidate no
+                      , withWarnings no
+                      ]
+
+getXPathText :: ArrowXml a => String -> a XmlTree String
+getXPathText xpath = getXPathTrees xpath //> getText
+
+atTag :: ArrowXml a => String -> a XmlTree XmlTree
+atTag tag = deep (isElem >>> hasName tag)
+
+parseXMLField :: ArrowXml a => a XmlTree Field
+parseXMLField = proc tree -> do
+    objs <- getXPathTrees "//field" -< tree
+    deep eachField -< objs
+
+eachField :: ArrowXml a => a XmlTree Field
+eachField = atTag "field" >>> proc tree -> do
+    field <- getXPathText "//name[1]" -< tree
+    haskellName <- (getXPathText "//haskell_name[1]" >>> arr Just) `orElse` constA Nothing -< tree
+    mappedType <- getXPathText "//mapped_type[1]" -< tree
+    nullable <- getAttrValue "nullable" -< tree
+    returnA -< Field field haskellName mappedType (nullable == "true")
+
+snakeToLowerCamel :: String -> String
+snakeToLowerCamel [] = []
+snakeToLowerCamel ('_':[]) = []
+snakeToLowerCamel ('_':x:xs) = toUpper x : snakeToLowerCamel xs
+snakeToLowerCamel str = f ++ snakeToLowerCamel next
+  where (f, next) = span (/= '_') str
+
+snakeToUpperCamel :: String -> String
+snakeToUpperCamel = upcase . snakeToLowerCamel
+  where
+    upcase [] = []
+    upcase (x:xs) = toUpper x : xs
+
+genTypeField :: String -> String -> Field -> T.Text
+genTypeField prefix commaOrBrace Field{..} =
+    [st|    #{commaOrBrace} #{prefix}#{name} :: #{mb}#{fieldMappedType}|]
+  where
+    name = snakeToUpperCamel $ fromMaybe fieldName fieldHaskellName
+    mb :: String
+    mb = if fieldNullable then "Maybe " else ""
+
+genFromJSON :: String -> Field -> T.Text
+genFromJSON prefix Field{..} =
+    [st|        #{prefix} o .:#{mb} "#{fieldName}"|]
+  where
+    mb :: String
+    mb = if fieldNullable then "?" else " "
+
+main :: IO ()
+main = do
+    (prefix:input:[]) <- getArgs
+    content <- L.readFile input
+    [tree] <- runX $ parseXML (L8.unpack content)
+    let fields = runLA parseXMLField tree
+        typeName = snakeToUpperCamel prefix
+
+    T.putStrLn . T.intercalate "\n" $
+        [ [st|data #{typeName} = #{typeName}|]
+        , T.intercalate "\n" $ map (\(field, cb) -> genTypeField prefix cb field) (zip fields ("{":repeat ","))
+        , "    }"
+        , ""
+        , [st|
+instance FromJSON #{typeName} where
+    parseJSON (Object o) =|]
+        , T.intercalate "\n" $ map (\(field, cb) -> genFromJSON cb field) (zip fields ((typeName ++ " <$>"):repeat (take (length typeName) (repeat ' ') ++ " <*>")))
+        , "    parseJSON _ = mzero"
+        ]
---
+++ attic/platformObject.hs
@@ -0,0 +1,79 @@
+{-# LANGUAGE Arrows #-}
+
+import qualified Data.ByteString.Lazy as L
+import qualified Data.ByteString.Lazy.Char8 as L8
+import Data.Char
+import Network.HTTP.Conduit
+import System.Environment
+import Text.XML.HXT.Core hiding (when)
+import Text.XML.HXT.XPath
+
+data Field = Field
+    { fieldName :: String
+    , fieldType :: String
+    , fieldDescription :: String
+    , fieldNullable :: Bool
+    } deriving Show
+
+parseHTML :: String -> IOStateArrow s b XmlTree
+parseHTML = readString [ withValidate no
+                       , withParseHTML yes
+                       , withWarnings no
+                       ]
+
+getXPathText :: ArrowXml a => String -> a XmlTree String
+getXPathText xpath = getXPathTrees xpath //> getText
+
+cleanStr :: String -> String
+cleanStr = reverse . dropWhile isSpace . reverse . dropWhile isSpace
+
+getHTMLTree
+  :: Request
+  -> Manager
+  -> IO [XmlTree]
+getHTMLTree req mgr = do
+  res <- httpLbs req mgr
+  let body = L8.unpack . responseBody $ res
+  runX $ parseHTML body
+
+platformObjects :: ArrowXml a => a XmlTree Field
+platformObjects = proc tree -> do
+    objs <- getXPathTrees "//table[@class='platform-object'][1]" -< tree
+    deep eachObj -< objs
+
+atTag :: ArrowXml a => String -> a XmlTree XmlTree
+atTag tag = deep (isElem >>> hasName tag)
+
+eachObj :: ArrowXml a => a XmlTree Field
+eachObj = atTag "tr" >>> proc tree -> do
+    field <- getXPathText "//th[1]" -< tree
+    typ <- getXPathText "//td[1]" -< tree
+    desc <- listA (getXPathText "//td[2]") -< tree
+    nullable <- (getXPathText "//td[2]/em[1]" >>> arr ((== "Nullable") . cleanStr)) `orElse` constA False -< tree
+    returnA -< Field (cleanStr field) (cleanStr typ) (cleanStr $ concat desc) nullable
+
+fieldElem :: ArrowXml a => a Field XmlTree
+fieldElem =
+    mkelem "field" [mkAttr (mkQName "" "nullable" "") (this >>> arr fieldNullable >>> arr (map toLower . show) >>> mkText)]
+        [ selem "name" [this >>> arr fieldName >>> mkText]
+        , selem "type" [this >>> arr fieldType >>> mkText]
+        , selem "mapped_type" [this >>> arr (mappedType . fieldType) >>> mkText]
+        , selem "description" [this >>> arr fieldDescription >>> mkText]
+        ]
+
+mappedType :: String -> String
+mappedType "Boolean" = "Bool"
+mappedType "String" = "Text"
+mappedType a = a
+
+main :: IO ()
+main = do
+    (input:output:[]) <- getArgs
+    content <- L.readFile input
+    [tree] <- runX $ parseHTML (L8.unpack content)
+    let fields = runLA platformObjects tree
+    _ <- runX $
+        root [] [selem "platform-object" [constL fields >>> fieldElem]]
+        >>>
+        writeDocument [withIndent yes] output
+    return ()
