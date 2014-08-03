05af63dc83074d0073c87f90398feb9fc0f348e7
Takahiro HIMURA <taka@himura.jp>
2014-08-03 21:48:11 +0900

    Add documents

--- Web/Twitter/Types/Lens/Types.hs
+++ Web/Twitter/Types/Lens/Types.hs
@@ -5,5 +5,13 @@ module Web.Twitter.Types.Lens.Types
        , SimpleLens
        ) where
 
+-- | A type alias of the lens.
+-- It is the same definition as the lens which introduced in
+-- <http://hackage.haskell.org/package/lens-4.3.3/docs/Control-Lens-Type.html#t:Lens>
 type Lens s t a b = forall f. Functor f => (a -> f b) -> s -> f t
+
+-- | Same as Lens' <http://hackage.haskell.org/package/lens-4.3.3/docs/Control-Lens-Type.html#t:Lens-39->
+--
+-- The type name with ' letter is not works well with clang cpp preprocessor,
+-- so we use "SimpleLens" instead of Lens'.
 type SimpleLens s a = Lens s s a a
