3c3e20a82bfa98ebe24241eb1c15a9bd517ad817
Takahiro HIMURA <taka@himura.jp>
2014-11-16 14:47:30 +0900

    Support lens >= 4.5

--- twitter-types-lens/Web/Twitter/Types/Lens/TH.hs
+++ twitter-types-lens/Web/Twitter/Types/Lens/TH.hs
@@ -9,7 +9,9 @@ import Control.Lens hiding (makeLenses)
 makeLenses :: Name -> Q [Dec]
 makeLenses = makeLensesWith $ lensRules & lensField .~ nameF
   where
-#if MIN_VERSION_lens(4, 4, 0)
+#if MIN_VERSION_lens(4, 5, 0)
+    nameF _ _ n = [TopName . mkName . nameBase $ n]
+#elif MIN_VERSION_lens(4, 4, 0)
     nameF _ n = [TopName . mkName . nameBase $ n]
 #elif MIN_VERSION_lens(4, 0, 0)
     nameF = Just
