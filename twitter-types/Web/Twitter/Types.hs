b23bbb473fe1532702abe4d1c8fe39763d870eab
Takahiro HIMURA <taka@himura.jp>
2015-01-30 02:10:30 +0900

    Support time>=1.5

--- twitter-types/Web/Twitter/Types.hs
+++ twitter-types/Web/Twitter/Types.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE OverloadedStrings, DeriveDataTypeable, RecordWildCards, DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings, DeriveDataTypeable, RecordWildCards, DeriveGeneric, CPP #-}
 
 module Web.Twitter.Types
        ( UserId
@@ -45,9 +45,13 @@ import Data.Aeson.Types (Parser)
 import Data.Data
 import Data.HashMap.Strict (HashMap, fromList, union)
 import Data.Text (Text, unpack, pack)
-import Data.Time
 import GHC.Generics
+#if MIN_VERSION_time(1, 5, 0)
+import Data.Time
+#else
+import Data.Time
 import System.Locale
+#endif
 
 newtype TwitterTime = TwitterTime { fromTwitterTime :: UTCTime }
 
@@ -79,7 +83,11 @@ twitterTimeFormat = "%a %b %d %T %z %Y"
 
 instance FromJSON TwitterTime where
     parseJSON = withText "TwitterTime" $ \t ->
+#if MIN_VERSION_time(1, 5, 0)
+        case parseTimeM True defaultTimeLocale twitterTimeFormat (unpack t) of
+#else
         case parseTime defaultTimeLocale twitterTimeFormat (unpack t) of
+#endif
             Just  d -> pure $ TwitterTime d
             Nothing -> fail $ "Could not parse twitter time. Text was: " ++ unpack t
 
