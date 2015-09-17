17fa9c8e9ea29dbeff108a8f936a5577e72c4d2f
Takahiro HIMURA <taka@himura.jp>
2015-09-17 10:26:51 +0900

    fix .travis/copy_stack_dist.sh

--- .travis/copy_stack_dist.sh
+++ .travis/copy_stack_dist.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
-# copy dist dir to ordinal path to support hpc-coveralls
+# copy mix and tix files to ordinal path to support hpc-coveralls
 
-cp -a twitter-types/$HASKELL_DIST_DIR twitter-types/dist
+STACK_DIST_DIR=twitter-types/$HASKELL_DIST_DIR
+HPC_DIR=twitter-types/dist/hpc/vanilla
+
+HPC_MIX_DIR=$HPC_DIR/mix/twitter-types-$(awk '/^version:/{print $2}' twitter-types/twitter-types.cabal)/
+
+mkdir -p $HPC_DIR
+
+mkdir -p $HPC_DIR/tix/tests
+cp $STACK_DIST_DIR/hpc/tests.tix $HPC_DIR/tix/tests
+
+mkdir -p $HPC_MIX_DIR
+cp -a $STACK_DIST_DIR/hpc/.hpc/* $HPC_MIX_DIR/
+
+
