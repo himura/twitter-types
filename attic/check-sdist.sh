56f93183a5f178c0542f7c7876d5bcec4822bac1
Takahiro HIMURA <taka@himura.jp>
2015-10-14 01:33:12 +0900

    travis: check-sdist.sh: do not use cabal command

--- attic/check-sdist.sh
+++ attic/check-sdist.sh
@@ -2,18 +2,18 @@
 
 BASEDIR="$PWD"
 TARGETDIR="$1"
+DIST="$(stack path | awk '/^dist-dir/ {print $2}')"
 
 cd "$TARGETDIR"
 
-PKGNAME="$(cabal info . | awk '/^\*/{print $2}')"
-SRCTGZ="${PKGNAME}.tar.gz"
-DIST="$(stack path | awk '/^dist-dir/ {print $2}')"
-
-if [ -f "${DIST%/}/$SRCTGZ" ]; then
+if [ -f ${DIST%/}/${TARGETDIR}-*.tar.gz ]; then
     cd "$DIST"
-    tar xvzf "$SRCTGZ"
+    srctgz=(${TARGETDIR}*.tar.gz)
+    srctgz="${srctgz[0]}"
+    pkgname="${srctgz%.tar.gz}"
+    tar xvzf "${srctgz}"
     cd "$BASEDIR/$TARGETDIR"
-    NG=$(git ls-tree -r HEAD | while read perm blob hash name; do [ -e "$DIST/$PKGNAME/$name" ] || echo "$name"; done)
+    NG=$(git ls-tree -r HEAD | while read perm blob hash name; do [ -e "$DIST/$pkgname/$name" ] || echo "$name"; done)
     if [ -n "$NG" ]; then
         echo "Missing files:"
         echo $NG
